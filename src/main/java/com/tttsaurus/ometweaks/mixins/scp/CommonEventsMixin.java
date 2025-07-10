336ce40553f71c54cecb4565966553fd5b55d665
tttsaurus <72607439+tttsaurus@users.noreply.github.com>
2025-07-10 18:46:35 +0800

    Bump to v1.5.3

--- src/main/java/com/tttsaurus/ometweaks/mixins/scp/CommonEventsMixin.java
+++ src/main/java/com/tttsaurus/ometweaks/mixins/scp/CommonEventsMixin.java
@@ -19,7 +19,6 @@ import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.world.World;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import org.spongepowered.asm.mixin.Mixin;
@@ -42,9 +41,7 @@ public class CommonEventsMixin
     @SubscribeEvent
     private static void onPlayerTick(TickEvent.PlayerTickEvent playerTickEvent, Operation<Void> original)
     {
-        World world = playerTickEvent.player.world;
-
-        if (world.isRemote) return;
+        if (playerTickEvent.player.world.isRemote) return;
         if (!(playerTickEvent.player instanceof EntityPlayerMP player)) return;
 
         SCPNegativeCapabilities capabilities = OME_Tweaks$negativeCapabilityCache.get(player.getUniqueID());
