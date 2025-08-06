690ead5add6ad126c1de6db3e10074295e7203d2
tttsaurus <72607439+tttsaurus@users.noreply.github.com>
2025-08-06 17:16:01 +0800

    Remove scp cap cache

--- src/main/java/com/tttsaurus/ometweaks/mixins/scp/CommonEventsMixin.java
+++ src/main/java/com/tttsaurus/ometweaks/mixins/scp/CommonEventsMixin.java
@@ -14,7 +14,6 @@ import alexiy.secure.contain.protect.events.CommonEvents;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.tttsaurus.ometweaks.integration.scp.SCPModule;
-import com.tttsaurus.ometweaks.integration.scp.capability.SCPNegativeCapabilities;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
@@ -22,17 +21,11 @@ import net.minecraft.util.EnumHand;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
+@SuppressWarnings("all")
 @Mixin(CommonEvents.class)
 public class CommonEventsMixin
 {
-    @Unique
-    private static final Map<UUID, SCPNegativeCapabilities> OME_Tweaks$negativeCapabilityCache = new HashMap<>();
-
     /**
      * @author tttsaurus
      * @reason To disable capabilities by config, and this method is modified from alexiy.secure.contain.protect.events.CommonEvents.onPlayerTick
@@ -44,52 +37,46 @@ public class CommonEventsMixin
         if (playerTickEvent.player.world.isRemote) return;
         if (!(playerTickEvent.player instanceof EntityPlayerMP player)) return;
 
-        SCPNegativeCapabilities capabilities = OME_Tweaks$negativeCapabilityCache.get(player.getUniqueID());
-        if (capabilities == null)
-        {
-            capabilities = new SCPNegativeCapabilities();
-            capabilities.sleepDeprivation = (ISleepDeprivationCapability)player.getCapability(Capabilities.SlEEP_DEPRIVATION, (EnumFacing)null);
-            capabilities.shadowInfestation = (IShadowInfestationCapability)player.getCapability(Capabilities.SHADOW_INFESTATION_CAPABILITY, (EnumFacing)null);
-            capabilities.bloodstone = (IBloodstoneCapability)player.getCapability(Capabilities.BLOODSTONE_CAPABILITY, (EnumFacing)null);
-            capabilities.infection = (Infection)player.getCapability(Capabilities.INFECTIONABLE, (EnumFacing)null);
-            capabilities.killedEntities = (IKilledEntitiesCapability)player.getCapability(Capabilities.KILLED_ENTITIES_CAPABILITY, (EnumFacing)null);
-            capabilities.cowbell = (ICowbellCapability)player.getCapability(Capabilities.COWBELL_CAPABILITY, (EnumFacing)null);
-            capabilities.lostItems = (ILostItemsCapability)player.getCapability(Capabilities.LOST_ITEMS_CAPABILITY, (EnumFacing)null);
-            OME_Tweaks$negativeCapabilityCache.put(player.getUniqueID(), capabilities);
-        }
+        ISleepDeprivationCapability sleepDeprivation = (ISleepDeprivationCapability)player.getCapability(Capabilities.SlEEP_DEPRIVATION, (EnumFacing)null);
+        IShadowInfestationCapability shadowInfestation = (IShadowInfestationCapability)player.getCapability(Capabilities.SHADOW_INFESTATION_CAPABILITY, (EnumFacing)null);
+        IBloodstoneCapability bloodstone = (IBloodstoneCapability)player.getCapability(Capabilities.BLOODSTONE_CAPABILITY, (EnumFacing)null);
+        Infection infection = (Infection)player.getCapability(Capabilities.INFECTIONABLE, (EnumFacing)null);
+        IKilledEntitiesCapability killedEntities = (IKilledEntitiesCapability)player.getCapability(Capabilities.KILLED_ENTITIES_CAPABILITY, (EnumFacing)null);
+        ICowbellCapability cowbell = (ICowbellCapability)player.getCapability(Capabilities.COWBELL_CAPABILITY, (EnumFacing)null);
+        ILostItemsCapability lostItems = (ILostItemsCapability)player.getCapability(Capabilities.LOST_ITEMS_CAPABILITY, (EnumFacing)null);
 
         if (SCPModule.DISABLE_SCP_SLEEP_DEPRIVATION_CAP)
-            capabilities.sleepDeprivation.cure(player);
+            sleepDeprivation.cure(player);
 
         if (SCPModule.DISABLE_SCP_SHADOW_INFESTATION_CAP)
         {
-            capabilities.shadowInfestation.setInfestationStage(0);
-            capabilities.shadowInfestation.setInfestationTime(-1);
+            shadowInfestation.setInfestationStage(0);
+            shadowInfestation.setInfestationTime(-1);
         }
 
         if (SCPModule.DISABLE_SCP_BLOODSTONE_CAP)
-            capabilities.bloodstone.resetBloodstoneState();
+            bloodstone.resetBloodstoneState();
 
         if (SCPModule.DISABLE_SCP_INFECTION_CAP)
         {
-            capabilities.infection.setActive(false);
-            capabilities.infection.setDuration(0);
+            infection.setActive(false);
+            infection.setDuration(0);
         }
 
         if (SCPModule.DISABLE_SCP_KILLED_ENTITIES_CAP)
-            capabilities.killedEntities.getStoredEntities().clear();
+            killedEntities.getStoredEntities().clear();
 
         if (SCPModule.DISABLE_SCP_COWBELL_CAP)
-            capabilities.cowbell.setHasRungCowbell(false);
+            cowbell.setHasRungCowbell(false);
 
         if (SCPModule.DISABLE_SCP_LOST_ITEMS_CAP)
-            capabilities.lostItems.getLostItems().clear();
+            lostItems.getLostItems().clear();
 
         if (!SCPModule.DISABLE_SCP_SLEEP_DEPRIVATION_CAP)
-            capabilities.sleepDeprivation.applySleepDeprivation(player);
+            sleepDeprivation.applySleepDeprivation(player);
         if (!SCPModule.DISABLE_SCP_BLOODSTONE_CAP)
             if (Utils.isPlayerInSurvivalMode(player))
-                capabilities.bloodstone.handleBloodstoneEffects(playerTickEvent.player);
+                bloodstone.handleBloodstoneEffects(playerTickEvent.player);
 
         ItemStack heldItem;
         SCPItem scpItem;
